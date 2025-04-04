82127a298ad8f01a69d9d12342019073ff7d23a6
aryanarora0 <aryan.arora.in@gmail.com>
2025-04-04 02:00:59 -0400

    Implemented number of epochs within training func

--- training.py
+++ training.py
@@ -1,19 +1,21 @@
 import numpy as np
 
-def train_epoch(model, train_loader, criterion, optimizer, device, loss_log, epoch=10):
+def train_epoch(model, train_loader, criterion, optimizer, loss_log, n_epoch=10):
     model.train()
-    total_loss = 0
-    for data, target in train_loader:
+    for epoch in range(n_epoch):
+        total_loss = 0
+        for data, target in train_loader:
+            optimizer.zero_grad()
+            outputs = model(data)
+            loss = criterion(outputs, target.unsqueeze(1))
+            loss.backward()
+            optimizer.step()
 
-        optimizer.zero_grad()
-        outputs = model(data)
-        loss = criterion(outputs, target.unsqueeze(1))
-        loss.backward()
-        optimizer.step()
+            total_loss += loss.item()
 
-        total_loss += loss.item()
+        avg_loss = total_loss / len(train_loader)
+        loss_log.append(avg_loss)
 
-    avg_loss = total_loss / len(train_loader)
-    loss_log.append(avg_loss)
+        print(f'Epoch {epoch+1}, Average Loss: {avg_loss:.4f}')
 
     return avg_loss
